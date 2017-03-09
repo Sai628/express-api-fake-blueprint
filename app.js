9783cad10ca48c0bcdf1530f4d03e99468db642d
Sai628 <sai100628@gmail.com>
2017-03-09 20:48:53 +0800

    code indent style

--- app.js
+++ app.js
@@ -14,23 +14,22 @@ var routes = require('./config/routes');
 var app = express();
 var fs = require('fs');
 
-
 // models loading
 var modelsPath = __dirname + '/app/models';
 var walk = function(path)
 {
-	fs.readdirSync(path).forEach(function(file) {
-		var modelFilePath = path + '/' + file;
-		var stat = fs.statSync(modelFilePath);
-		if (stat.isFile()){
-			if (/(.*)\.(js)/.test(file)){
-				require(modelFilePath);
-			}
-		}
-		else if (stat.isDirectory()){
-			walk(modelFilePath);
-		}
-	});
+    fs.readdirSync(path).forEach(function(file) {
+        var modelFilePath = path + '/' + file;
+        var stat = fs.statSync(modelFilePath);
+        if (stat.isFile()) {
+            if (/(.*)\.(js)/.test(file)) {
+                require(modelFilePath);
+            }
+        }
+        else if (stat.isDirectory()) {
+            walk(modelFilePath);
+        }
+    });
 };
 walk(modelsPath);
 
@@ -41,74 +40,72 @@ app.use(flash());
 app.use(express.cookieParser());
 app.use(express.favicon());
 app.use(bodyParser.json({
-	extended: true,
-	parameterLimit: 10000,
-	limit: 1024 * 1024 * 10
+    extended: true,
+    parameterLimit: 10000,
+    limit: 1024 * 1024 * 10
 }));
 app.use(bodyParser.urlencoded({
-	extended: true,
-	parameterLimit: 10000,
-	limit: 1024 * 1024 * 10
+    extended: true,
+    parameterLimit: 10000,
+    limit: 1024 * 1024 * 10
 }));
 app.use(express.bodyParser({
-	uploadDir: __dirname + '/public/upload'
+    uploadDir: __dirname + '/public/upload'
 }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.locals.moment = require('moment');
 
-
 var logRequest = expressWinston.logger({
-	transports: [
-		new winston.transports.Console({
-			json: true,
-			colorize: true
-		})
-	],
-	meta: true,
-	expressFormat: true,
-	colorStatus: true,
-	ingoreRoute: function(req, res) { return false; }
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        })
+    ],
+    meta: true,
+    expressFormat: true,
+    colorStatus: true,
+    ingoreRoute: function(req, res) { return false; }
 })
 
 function logResponseBody(req, res, next) 
 {
-	var oldWrite = res.write;
-	var oldEnd = res.end;
-  	var chunks = [];
+    var oldWrite = res.write;
+    var oldEnd = res.end;
+    var chunks = [];
 
-  	res.write = function (chunk) {
-    	chunks.push(new Buffer(chunk));
-	    oldWrite.apply(res, arguments);
-  	};
+    res.write = function (chunk) {
+        chunks.push(new Buffer(chunk));
+        oldWrite.apply(res, arguments);
+    };
 
-  	res.end = function (chunk) {
-    	if (chunk) {
-      		chunks.push(new Buffer(chunk));
-		}
+    res.end = function (chunk) {
+        if (chunk) {
+            chunks.push(new Buffer(chunk));
+        }
 
-    	var body = Buffer.concat(chunks).toString('utf8');
-    	console.log(body);
-    	oldEnd.apply(res, arguments);
-  	};
+        var body = Buffer.concat(chunks).toString('utf8');
+        console.log(body);
+        oldEnd.apply(res, arguments);
+    };
 
-  	next();
+    next();
 }
 
 // development only
 if ('development' == app.get('env'))
 {
-	app.set('showStackError', true);
-	app.use(express.errorHandler());
-	app.use(logRequest);
-	app.use(logResponseBody);
-	app.use(express.logger(':method :url :status :response-time ms'));
-	app.locals.pretty = true;
+    app.set('showStackError', true);
+    app.use(express.errorHandler());
+    app.use(logRequest);
+    app.use(logResponseBody);
+    app.use(express.logger(':method :url :status :response-time ms'));
+    app.locals.pretty = true;
 }
 
 routes(app);
 app.listen(port, function()
 {
-	console.log('Express server listening on port ' + port);
+    console.log('Express server listening on port ' + port);
 });
-
